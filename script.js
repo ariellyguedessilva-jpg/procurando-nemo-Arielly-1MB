eecf959ed2c43f3ec42337e1345e439b974f1cab
ariellyguedessilva-jpg <arielly.guedes.silva@escola.pr.gov.br>
2025-11-06 11:52:38 -0300

    color

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const avanca = document. querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventlistener('click',function(){
-      const atual = document.querySelector('.ativa');
-      const proximoPasso = 'passo-' + this.getAtribute('data-proximo');
+      const atual = document.querySelector('.ativo');
+      const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
        atual.classList.remove('ativo');
        document.getElementById(proximoPasso).classList.add('ativo');
